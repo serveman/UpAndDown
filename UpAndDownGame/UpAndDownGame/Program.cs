3d9843a738c5fa5a65fa02d15fbaaae7ea1a87d4
serveman <serveman6325@gmail.com>
2024-08-23 13:45:23 +0900

    fix: 솔루션 생성 (초기 환경 재정비)

--- UpAndDownGame/UpAndDownGame/Program.cs
+++ UpAndDownGame/UpAndDownGame/Program.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UpAndDownGame.Main;
 
-namespace UpAndDown
+namespace UpAndDownGame
 {
     class Program
     {
         static void Main(string[] args)
         {
+            new UpAndDown();
         }
     }
 }
