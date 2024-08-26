b54a0c6aa5458f098554ef1ce33fc9a18eda45ba
serveman <serveman6325@gmail.com>
2024-08-26 18:32:15 +0900

    feat: 난이도별 Count 추가

--- UpAndDownGame/UpAndDownGame/User/member.cs
+++ UpAndDownGame/UpAndDownGame/User/member.cs
@@ -1,9 +1,10 @@
+using System.Collections.Generic;
 
 namespace UpAndDown.User
 {
     public struct Member
     {
         public string Name { get; set; }
-        public Count PlayCount { get; set; }
+        public List<Count> PlayCountList { get; set; }
     }
 }
